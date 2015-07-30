919b0a8cddbf05a8c652efaef24b48221917c8a9
Anis Abid <anis.abid-ext@amundi.com>
2015-07-30 18:00:34 +0200

    sov

--- www/js/main/directives/card.directive.js
+++ www/js/main/directives/card.directive.js
@@ -3,7 +3,7 @@
 (function () {
 
   angular.module('trombiApp')
-    .directive('amCard', [function () {
+    .directive('amCard', ['$http', function ($http) {
 
       return {
         restrict: 'E',
@@ -15,9 +15,37 @@
         },
         link: function (scope, elem) {
 
-          var avatar = elem.children(1);
-          console.log(avatar);
-          avatar.css({backgroundImage: 'url(' + scope.data.url1 + ')'});
+          var avatar = angular.element(elem[0].querySelectorAll('.avatar')[0]);
+
+          // preload image
+          var img = new Image();
+          img.src = scope.data.url1;
+
+          var unbind = function () {
+            angular.element(img).off();
+          };
+
+
+          angular.element(img).on('load', function () {
+            avatar.css({
+              backgroundImage: 'url(' + scope.data.url1 + ')'
+            });
+            unbind();
+          });
+
+          angular.element(img).on('error', function () {
+            errorImg();
+            unbind();
+          });
+
+          var errorImg = function () {
+
+            return true;
+          };
+
+          //var avatar = angular.element(elem[0].querySelectorAll('.avatar')[0]);
+
+          //avatar.css({backgroundImage: 'url(' + scope.data.url1 + ')'});
 
           //var elem = elem.children('.avatar');
 
